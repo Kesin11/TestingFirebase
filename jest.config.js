eee2dae6a0c538443f086450ee7e3281da1e5f3c
Kenta Kase <kesin1202000@gmail.com>
2019-03-04 00:23:54 +0900

    Store test results in CircleCI

--- jest.config.js
+++ jest.config.js
@@ -95,7 +95,7 @@ module.exports = {
   // projects: null,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: ["default"],
 
   // Automatically reset mock state between every test
   // resetMocks: false,
