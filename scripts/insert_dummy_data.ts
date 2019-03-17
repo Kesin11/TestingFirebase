18a8a5631d9df9728876a86c22fe560525ef1455
Kenta Kase <kesin1202000@gmail.com>
2019-03-17 15:45:01 +0900

    Allow running insert dummy data script in dev project

--- scripts/insert_dummy_data.ts
+++ scripts/insert_dummy_data.ts
@@ -2,8 +2,13 @@ import admin, { ServiceAccount } from 'firebase-admin'
 import serviceAccount from '../service_account.json'
 import { insertUserFixutre } from '../src/test_util'
 
-if (serviceAccount.project_id !== 'testing-firebase-test') {
-  throw new Error('Insert dummy data allowed test project only! Replace to test project service_account.json')
+// 環境変数でprod, dev, testを切り替える仕組みが整っていればそれで判定してもOK
+if (
+  serviceAccount.project_id !== 'testing-firebase-dev' &&
+  serviceAccount.project_id !== 'testing-firebase-test'
+  ) {
+  console.log(`project_id: ${serviceAccount.project_id}`)
+  throw new Error('Insert dummy data allowed "dev" or "test" project only! Replace to test project service_account.json')
 }
 
 const app = admin.initializeApp({
